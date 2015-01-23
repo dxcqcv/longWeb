fa652eedebaf2ff36336c616acd534b97564633e
Roy <dxcqcv@gmail.com>
2015-01-23 14:36:31 +0800

    update rebuilt jzq2

--- works/ENN/dataHub/jzq/js/index.js
+++ works/ENN/dataHub/jzq/js/index.js
@@ -1,121 +1,14 @@
-
 (function(){
 // for content height
     var winHei = $(window).height()
       , conHei = winHei - 72
+
     $('.content').height(conHei)
 
-}());
-// for test
-function tt(op) { return op.tt ? op.tt : 11}
+    $(document).on('click','.content-left li',s)
+    $(document).on('click','.head-nav li',m)
+    //$(document).on('click','.head-nav li',mainNav)
 
-Navigation.prototype.mod = function(){
-this.cat = 111
-    alert(this.cat) 
-};
-function Navigation() {
-    this.pos = null;
-    this.cat = 222;
-}
-function MainNav() {
-    Navigation.call(this)
-}
-MainNav.prototype = new Navigation();
-
-function SideNav() {
-    Navigation.call(this)
-}
-SideNav.prototye = Navigation.prototype;
-
-    to();
-function to() {
-
-var n = new Navigation();  console.log(n); n.mod(); }
-
-function navSel() {
-    var $this = $(this)
-      , cat = $this.data('cat') // nav category 
-      , ztxxTab = $('#ztxxTable')
-      , dkpzTab = $('#dkpzTable')
-    $this.addClass('active').siblings('li').removeClass('active')
-    switch(cat) {
-        case 0:
-            ztxxTab.removeClass('hide')            
-            dkpzTab.addClass('hide')            
-            break
-        case 1:
-            ztxxTab.addClass('hide')            
-            dkpzTab.removeClass('hide')            
-            break
-    }
-}
-function cmd11Done(data) {
-    var escape = JSON.parse(data);
-    $('#webportV').data(escape.webport);
-    $('#modbusportV').html(escape.modbusport);
-}
-function failFn() {
-    console.log('error')
-}
-function sidebarSel() {
-    var $this = $(this)
-      , type = $this.data('type') || 0
-      , num = $this.data('num') 
-      , subTitle = $('#contSubNavStat')
-      , table = $('#ztxxTable') 
-      , tbody = table.find('tbody#ztxxNetTbody')
-      , th = table.find('th')
-      , cmd11 = new Request({data:{cmd:11},done:cmd11Done,fail:failFn});
-
-    $this.addClass('active').siblings('li').removeClass('active')
-
-    if(type === 0) {
-       th.eq(0).text('设备号') 
-       th.eq(1).text('设备') 
-    } else {
-       th.eq(0).text('项目') 
-       th.eq(1).text('端口') 
-    }
-    switch(num) {
-        case -1:
-            subTitle.text('网络')
-            tbody.removeClass('hide').siblings('tbody').addClass('hide')
-            cmd11.start();
-            break
-        case 0:
-            subTitle.text('串口1')
-            tbody.addClass('hide').siblings('tbody').removeClass('hide')
-            break
-        case 1:
-            subTitle.text('串口2')
-            tbody.addClass('hide').siblings('tbody').removeClass('hide')
-            break
-        case 2:
-            subTitle.text('串口3')
-            tbody.addClass('hide').siblings('tbody').removeClass('hide')
-            break
-        case 3:
-            subTitle.text('串口4')
-            tbody.addClass('hide').siblings('tbody').removeClass('hide')
-            break
-        case 4:
-            subTitle.text('串口5')
-            tbody.addClass('hide').siblings('tbody').removeClass('hide')
-            break
-        case 5:
-            subTitle.text('串口6')
-            tbody.addClass('hide').siblings('tbody').removeClass('hide')
-            break
-        case 6:
-            subTitle.text('串口7')
-            tbody.addClass('hide').siblings('tbody').removeClass('hide')
-            break
-        case 7:
-            subTitle.text('串口8')
-            tbody.addClass('hide').siblings('tbody').removeClass('hide')
-            break
-    }
-}
 
 // for ajax 
 function Request(opt) {
@@ -149,5 +42,141 @@ Request.prototype.start = function() {
             self.fail()
         })
     }
-    $(document).on('click','.content-left li',sidebarSel)
-    $(document).on('click','.head-nav li',navSel)
+function Navigation() {
+    this.pos = [[0,1,2,3,4,5,6,7,8],[0,1,2,3,4,5,6,7,8],[0,1,2,3,4,5,6,7],[0],[0,1,2,3,4]];
+    this.cat = 0;
+    this.side = 0;
+}
+Navigation.prototype = {
+    mainNav: function(that) {
+        var self = this
+          , curPos = 0 
+        self.navShow(that) 
+
+        self.cat= that.data('cat')
+
+curPos = self.pos[self.cat][self.side]
+
+        self.navShow($('#sideNav').find('li').eq(curPos))
+       
+        console.log(self.cat)
+  }
+  , navShow: function(nav) {
+        nav.addClass('active').siblings('li').removeClass('active')
+  }
+  , sideNav: function(that) {
+        var self = this
+        self.side = that.data('side')
+        self.navShow(that) 
+
+        console.log(self.side)
+  }
+}
+
+// instance nav
+    var m = new Navigation() 
+function m() {
+   var $this = $(this)
+   m.mainNav($this);
+}
+function s() {
+    var $this = $(this)
+    m.sideNav($this); 
+}
+function mainNav() {
+    var $this = $(this)
+      , cat = $this.data('cat') // nav category 
+      , ztxxTab = $('#ztxxTable')
+      , dkpzTab = $('#dkpzTable')
+    $this.addClass('active').siblings('li').removeClass('active')
+    switch(cat) {
+        case 0:
+            ztxxTab.removeClass('hide')            
+            dkpzTab.addClass('hide')            
+            break
+        case 1:
+            ztxxTab.addClass('hide')            
+            dkpzTab.removeClass('hide')            
+            break
+    }
+    category = cat
+    console.log('category is ' +category)
+}
+function cmd11Done(data) {
+    var escape = JSON.parse(data);
+    $('#webportV').data(escape.webport);
+    $('#modbusportV').html(escape.modbusport);
+}
+function failFn() {
+    console.log('error')
+}
+function sideNav() {
+    var $this = $(this)
+      , type = $this.data('type') || 0
+      , pos = position ? position : $this.data('pos') // save prev pos 
+      , subTitle = $('#contSubNavStat')
+      , table = $('#ztxxTable') 
+      , tbody = table.find('tbody#ztxxNetTbody')
+      , th = table.find('th')
+      , cmd11 = new Request({data:{cmd:11},done:cmd11Done,fail:failFn});// instance Request for cmd11
+
+    $this.addClass('active').siblings('li').removeClass('active')
+
+    if(category == null || category == 0)
+    {
+        if(type === 0) {
+           th.eq(0).text('设备号') 
+           th.eq(1).text('设备') 
+        } else {
+           th.eq(0).text('项目') 
+           th.eq(1).text('端口') 
+        }
+        switch(pos) {
+            case -1:
+                subTitle.text('网络')
+                tbody.removeClass('hide').siblings('tbody').addClass('hide')
+                cmd11.start();
+                break
+            case 0:
+                subTitle.text('串口1')
+                tbody.addClass('hide').siblings('tbody').removeClass('hide')
+                break
+            case 1:
+                subTitle.text('串口2')
+                tbody.addClass('hide').siblings('tbody').removeClass('hide')
+                break
+            case 2:
+                subTitle.text('串口3')
+                tbody.addClass('hide').siblings('tbody').removeClass('hide')
+                break
+            case 3:
+                subTitle.text('串口4')
+                tbody.addClass('hide').siblings('tbody').removeClass('hide')
+                break
+            case 4:
+                subTitle.text('串口5')
+                tbody.addClass('hide').siblings('tbody').removeClass('hide')
+                break
+            case 5:
+                subTitle.text('串口6')
+                tbody.addClass('hide').siblings('tbody').removeClass('hide')
+                break
+            case 6:
+                subTitle.text('串口7')
+                tbody.addClass('hide').siblings('tbody').removeClass('hide')
+                break
+            case 7:
+                subTitle.text('串口8')
+                tbody.addClass('hide').siblings('tbody').removeClass('hide')
+                break
+        }
+    } else if( category == 1) {
+
+    }
+    position = pos
+    console.log('position is ' +position)
+}
+
+
+}());
+
