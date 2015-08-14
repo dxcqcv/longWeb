2562326c055385b953d3607a456f0677dee7a346
Roy <dxcqcv@gmail.com>
2015-08-14 17:11:46 +0800

    qxj

---
+++ works/ENN/qixiuju/js/qxjJs.js
@@ -0,0 +1,29 @@
+(function(){
+    var secondNav = $('.qxj-header > ul > li > ul')
+    secondNav
+            .addClass('qxjHide')//隐藏二级导航
+            .on('mouseover',function(){
+                $(this).removeClass('qxjHide').addClass('qxjBlock');
+            }) 
+            .on('mouseout',function(){
+                $(this).removeClass('qxjBlock').addClass('qxjHide');
+            }); 
+
+    function Nav() {}
+    $.extend(Nav.prototype, {
+        hover: function(opt) {
+            var ele = opt.ele ? opt.ele : $(this)
+
+            ele
+                .on('mouseover',function(){
+                    $(this).siblings('ul').removeClass('qxjHide').addClass('qxjBlock');
+                })
+                .on('mouseout',function(){
+                    $(this).siblings('ul').removeClass('qxjBlock').addClass('qxjHide');
+                });
+        }
+    });
+    var headerNav = new Nav()
+    headerNav.hover({ele:$('.qxj-header > ul > li > a')})
+
+}());
