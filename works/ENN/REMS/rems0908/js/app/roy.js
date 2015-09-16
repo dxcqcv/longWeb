bf40903ec64a6204a352ab84ac811cb038fb0aa3
Roy <dxcqcv@gmail.com>
2015-09-16 17:06:00 +0800

    fixed ie bug and nav

--- works/ENN/REMS/rems0908/js/app/roy.js
+++ works/ENN/REMS/rems0908/js/app/roy.js
@@ -2,14 +2,29 @@ var royfunction = function() {
     var 
         navButton = $('.my-nav').children('li').children('a'),
         myCont = $('.my-content')
+        myShow = $('.my-show')
+        mySection = $('.my-section')
         ;
+        //首页导航高度
     navButton.on('click', function(){
-        var indexNav = $(this).data('show');        
+        var indexNav = $(this).data('hight');        
         if(indexNav === 1) {
             myCont.removeClass('one-nav').addClass('two-nav');        
         } else {
             myCont.removeClass('two-nav').addClass('one-nav');        
         } 
     });
+    //导航选择
+    myShow.on('click', function(){
+        var show = $(this).data('show');                
+        $.each(mySection, function(i,k){
+            var $this = $(this);
+            if($this.attr('id') == show) {
+                $this.siblings('div').addClass('hide').end().removeClass('hide');
+            }
+        });
+    });
 
+    var d = window.location.search;
+    alert(d);
 };
