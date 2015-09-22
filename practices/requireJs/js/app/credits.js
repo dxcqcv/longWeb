074a8d862daa2a2b123c623bd22c05c646d08557
dxcqcv <dxcqcv@gmail.com>
2015-09-23 07:51:41 +0800

    add require JS

---
+++ practices/requireJs/js/app/credits.js
@@ -0,0 +1,10 @@
+define(function(){
+    return {
+        getCredits: function() {
+            console.log('Function : getCredits');
+
+            var credits = '100';
+            return credits;
+        }
+    }
+});
